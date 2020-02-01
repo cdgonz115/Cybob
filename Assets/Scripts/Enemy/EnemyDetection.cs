3a9482c9270d0e8fb118aeb7d9cea5cfa20cd6f4
Phyrol <nitehawkk123@gmail.com>
2020-02-01 13:18:22 -0700

    PlayerCollisions script
    - Added player collision script and when they enter a GravityRoom the keys alpha increases and drops when leaving

---
+++ Assets/Scripts/Enemy/EnemyDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    public float lookRadius = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    public RawImage gravityKey;
+
+    private Color keyColor;
+
+    private void Start()
+    {
+        keyColor = gravityKey.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "GravityRoom")
+        {
+            keyColor.a = 1f;
+
+            gravityKey.color = keyColor;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "GravityRoom")
+        {
+            keyColor.a = 0.3f;
+
+            gravityKey.color = keyColor;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Item")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
