468cab722b6b494c05fc9aaf3f80b825999464ab
propredator23 <christiandash@hotmail.com>
2020-02-02 07:40:46 -0700

    updated enemies

--- Assets/Scripts/Enemy/ResetPOS.cs
+++ Assets/Scripts/Enemy/ResetPOS.cs
@@ -6,6 +6,6 @@ public class ResetPOS : MonoBehaviour
 {
     private void Start()
     {
-        this.gameObject.transform.position = new Vector3(0, 0, 0);
+        this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
     }
 }
