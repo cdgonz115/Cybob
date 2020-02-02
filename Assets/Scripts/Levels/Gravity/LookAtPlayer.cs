01a0b27b5431a1b0212b5db9aaf27f87abeae394
propredator23 <christiandash@hotmail.com>
2020-02-02 04:13:38 -0700

    added some 3d assets

---
+++ Assets/Scripts/Levels/Gravity/LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Camera camera1;
+    public Camera camera2;
+    private void FixedUpdate()
+    {
+        if (player.transform.position.z > 47)
+        {
+            camera1.enabled=false;
+            camera2.enabled=true;
+        }
+        transform.LookAt(player.transform.position);
+    }
+}
