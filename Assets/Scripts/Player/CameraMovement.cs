5d89d7b083ae99a0fb13baa92839aea639b23c82
propredator23 <christiandash@hotmail.com>
2020-02-02 04:57:39 -0700

    UpdatePlayer prefab

---
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private string xInput, yInput;
+    [SerializeField] private float mouseSensitivity;
+
+    private float xClamp;
+    [SerializeField] private Transform player;
+
+    CursorLockMode locked = CursorLockMode.Locked;
+    CursorLockMode free = CursorLockMode.None;
+
+    Movement pm;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    private void Awake()
+    {
+        pm = gameObject.GetComponentInParent<Movement>();
+        LockCursorToCenter();
+        xClamp = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+    private void LockCursorToCenter()
+    {
+        Cursor.lockState = locked;
+    }
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(xInput) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(yInput) * mouseSensitivity * Time.deltaTime;
+
+        xClamp += mouseY;
+
+        if (xClamp > 90.0f)
+        {
+            xClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xClamp < -90.0f)
+        {
+            xClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        player.Rotate(Vector3.up * mouseX);
+    }
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
