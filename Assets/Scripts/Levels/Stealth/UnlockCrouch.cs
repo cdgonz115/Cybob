5d2c6c075e2dd7e149e7ad4780e8a4858e7e8bac
propredator23 <christiandash@hotmail.com>
2020-02-02 07:27:37 -0700

    Sneak Level

---
+++ Assets/Scripts/Levels/Stealth/UnlockCrouch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCrouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+
+        other.GetComponentInParent<PlayerCrouch>().canCrouch = true;
+        Destroy(transform.gameObject);
+    }
+}
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,10 +6,21 @@ public class Movement : MonoBehaviour
 {
     public string verticalInput = "Vertical";
     public string horizontalInput = "Horizontal";
+    [SerializeField] private KeyCode jumpKey;
     public float runSpeed;
     public float movementSpeed = 4;
+    public float gravityCoefficient;
+    public float fallCoefficent;
+    public float jumpStrength;
     public float timer;
+    public float frictionCoefficient;
     public bool forwardUnlocked;
+    private bool jumping;
+    public bool grounded = true;
+    private float gravity;
+    public int jumps = 10;
+    public bool canJump = false;
+    public CapsuleCollider playerCollider;
 
     public Rigidbody rb;
     // Start is called before the first frame update
@@ -22,6 +33,12 @@ public class Movement : MonoBehaviour
     void FixedUpdate()
     {
         movement();
+        if (grounded && !Physics.Raycast(playerCollider.bounds.center, Vector3.down, playerCollider.bounds.extents.y + 0.5f))
+        {
+            gravity = fallCoefficent;
+            grounded = false;
+        }
+        JumpInput();
     }
     private void movement()
     {
@@ -30,5 +47,31 @@ public class Movement : MonoBehaviour
 
         rb.velocity = (forwardUnlocked ? (forward + lateral).normalized : (lateral).normalized) * (Input.GetKey(KeyCode.LeftShift) ? runSpeed : movementSpeed) + new Vector3(0, rb.velocity.y, 0);
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Vector3.Dot(collision.contacts[0].normal, Vector3.up) > 0.05)
+        {
+            grounded = true;
+            gravity = 0;
+            jumps = 1;
+        }
+    }
 
+    private void JumpInput()
+    {
+
+        if (Input.GetKeyDown(jumpKey) && jumps > 0 && canJump)
+        {
+            rb.AddForce(new Vector3(0, jumpStrength * 100, 0));
+            //b.velocity = new Vector3(rb.velocity.x, jumpStrength, rb.velocity.z);
+            rb.velocity /= frictionCoefficient;
+            jumps--;
+        }
+        else if (!grounded)
+        {
+                if (gravity < fallCoefficent * 4) gravity /= gravityCoefficient;
+                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - gravity, rb.velocity.z);
+        }
+
+    }
 }
